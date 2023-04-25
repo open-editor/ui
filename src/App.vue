878bcfe8ce48e94ac6d7f086eaded064dbcfafbb
alex Ko <konishchuk.sasha@gmail.com>
2023-04-26 00:27:50 +0300

    add tailwind and sign-up page

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/sign-up">Sign up</RouterLink>
       </nav>
     </div>
   </header>
