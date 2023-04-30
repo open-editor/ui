1c237689911cfa9f75ff4012ae8a9e1713d1a6b5
alex Ko <konishchuk.sasha@gmail.com>
2023-04-30 23:50:48 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,50 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="w-full  bg-slate-50 shadow-md flex px-8 py-2 fixed z-10 mx-auto dark:bg-gray-800 ">
+    <img alt="Vue logo" class="logo block" src="@/assets/logo.svg" width="60" height="60"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="wrapper w-full flex items-center justify-between px-8">
+      <div class="project-name">
+        <h1 class="green font-medium text-4xl"> Open Editor</h1>
+<!--        <h3 class="text-lg">Document editor with real-time collaboration capabilities</h3>-->
+    </div>
+        <nav class="ml-auto flex items-center">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/log-in" v-if="!isLogin">Login</RouterLink>
+            <div class="btn-signup ml-1" v-if="!isLogin">
+                <RouterLink to="/sign-up"
+                            class="text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                    Sign up
+                </RouterLink>
+            </div>
+            <div class="user-icon cursor-pointer" v-if="isLogin">
+                <img src="@/assets/user-icon.png" class="w-11 h-8">
+            </div>
+        </nav>
+        <div class="buttons flex">
+            <div class="btn-login">
+            </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/sign-up">Sign up</RouterLink>
-      </nav>
+        </div>
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const isLogin = true;
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<style scoped lang="scss">
+nav > a{
+  @apply py-2 px-4 lg:px-5 py-2 lg:py-2.5 duration-300;
+  &:hover{
+    @apply bg-gray-100 rounded-md;
   }
 }
+
 </style>
