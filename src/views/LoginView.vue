3b6f626e39f07fe7ffbc2ce5e1505be1579dd482
dddarkasss <dashka.gets@gmail.com>
2023-04-30 20:15:28 +0300

    login page with bg

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-[url('/images/bg.jpg')] bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply bg-opacity-60">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
