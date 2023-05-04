a1186fed63fdaa053bf87958f4fb3a20c3a06e77
alex Ko <konishchuk.sasha@gmail.com>
2023-05-04 15:59:12 +0300

    chenge header and sign-up page

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,7 +1,7 @@
 <template>
     <span>
         <label :for="inputInfo.name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ inputInfo.label }}</label>
-        <input :type="inputInfo.name" :name="inputInfo.name" :id="inputInfo.name" :placeholder="inputInfo.placeholder" required
+        <input :type="inputInfo.type || inputInfo.name" :name="inputInfo.name" :id="inputInfo.name" :placeholder="inputInfo.placeholder" required
                autocomplete="on" :value="inputInfo.value" @input="onInput" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </span>
 </template>
@@ -11,6 +11,7 @@
     interface userInfo{
         label: string,
         name: string,
+        type?: string,
         placeholder: string,
         value: string,
 
